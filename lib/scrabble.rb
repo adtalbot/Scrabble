c369c9534e510a2473aff1ffae2dbeb79c59fcfa
Audrie Talbot & Jeff Lalic <student@epicodus.com>
2015-04-22 13:47:50 -0700

    All letters working

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,9 +1,9 @@
 class String
   define_method(:scrabble) do
-    letters = self.split("")
+    letters = self.downcase().split("")
     total_score = 0
     letters.each() do |letter|
-      scrabble_key = { "e" => 1 }
+      scrabble_key = { "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1, "d" => 2, "g" => 2, "b" => 3, "c" => 3, "m" => 3, "p" => 3, "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4, "k" => 5, "j" => 8, "x" => 8, "q" => 10, "z" => 10 }
       score = scrabble_key.fetch(letter)
       total_score += score
     end
--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -5,8 +5,8 @@ Capybara.app = Sinatra::Application
 describe('the scrabble path', {:type => :feature}) do
   it('processes the user word entry and returns the Scrabble score') do
     visit('/')
-    fill_in('word_score', :with => 'e') #<--Test different word later
+    fill_in('word_score', :with => 'e')
     click_button('Submit')
-    expect(page).to have_content(1) # What does (page) do?
+    expect(page).to have_content(1)
   end
 end
